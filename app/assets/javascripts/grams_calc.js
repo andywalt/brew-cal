bd0b206b1e9f5c929e43d4c06159dc3649c99135
Andy Walters <andywalters42@gmail.com>
2019-08-24 14:32:47 -0700

    updated Git with changes

--- app/assets/javascripts/grams_calc.js
+++ app/assets/javascripts/grams_calc.js
@@ -1,8 +1,8 @@
 function finalCoffee(){
-	document.getElementById("coffeeLiquidFinal").innerHTML=document.getElementById("coffeLiquidMake").value;
+	document.getElementById("coffeeLiquidFinal").innerHTML=document.getElementById("coffedLiquidMake").value;
 };
 function calcbrew(amount){
 	amount=parseFloat(amount);
-	var brewCalc = 15.555555556/amount;
+	var brewCalc = 5 * amount;
 	document.getElementById("coffeeBeanUse").innerHTML=brewCalc;
 };
